64eebe26a61ed23e591006bf1cca23b3f3989e74
rhayssakramer <rhayssakramer@hotmail.com>
2025-09-19 12:48:24 -0300

    Criação do projeto backend com Java 21.0.8 e Springboot 3.5.6

---
+++ projeto-memuvie/src/main/java/com/example/projeto_memuvie/ProjetoMemuvieApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_memuvie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMemuvieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMemuvieApplication.class, args);
+	}
+
+}
